5f1f259827a7486120dea92cf6b680027fda00fb
Juan Pablo Mejia <juanpmd@hotmail.com>
2021-12-28 09:28:29 -0500

    feat(general): update package.js structure

--- package.js
+++ package.js
@@ -1,26 +1,19 @@
 Package.describe({
   name: 'codifytools:accounts-linkedin',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: 'Login service for Facebook accounts',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  version: '1.0.3',
+  summary: 'Login service for Linkedin accounts',
+  git: 'https://github.com/codifytools/meteor-accounts-linkedin',
   documentation: null
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('2.3');
-
   api.use('ecmascript');
   api.use('accounts-base', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('codifytools:linkedin-oauth');
+  api.use('codifytools:linkedin-oauth@1.0.3');
   api.imply('codifytools:linkedin-oauth');
-
-  api.use('http', ['client', 'server'])
 
   api.addFiles("linkedin.js");
 });
